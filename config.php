0642b07f844893458eaf47c77da8083820120539
Ario Widiatmoko <ariowidiatmoko@gmail.com>
2016-11-10 22:55:11 +0700

    Database Configuration
    Configuring database for hosting purpose

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ Config::set('DEFAULT_ROUTE', 'main');
 Config::set('DEFAULT_ACTION', 'index');
 
 // Configure Data Base
-Config::set('DB.HOST', 'localhost');
-Config::set('DB.USER', 'root');
-Config::set('DB.PASSWORD', '');
-Config::set('DB.NAME', 'codeballad');
+Config::set('DB.HOST', 'mysql.idhostinger.com');
+Config::set('DB.USER', 'u835915073_ario');
+Config::set('DB.PASSWORD', 'QWE123!#');
+Config::set('DB.NAME', 'u835915073_cb');
