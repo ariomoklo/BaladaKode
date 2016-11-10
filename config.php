bd372ada324531a68975190ee5bbe00816e215f7
Ario Widiatmoko <ariowidiatmoko@gmail.com>
2016-11-10 22:57:52 +0700

    Database Problem
    Fixing problems

--- config.php
+++ config.php
@@ -12,5 +12,5 @@ Config::set('DEFAULT_ACTION', 'index');
 // Configure Data Base
 Config::set('DB.HOST', 'mysql.idhostinger.com');
 Config::set('DB.USER', 'u835915073_ario');
-Config::set('DB.PASSWORD', 'QWE123!#');
+Config::set('DB.PASSWORD', 'QWE123!@#');
 Config::set('DB.NAME', 'u835915073_cb');
